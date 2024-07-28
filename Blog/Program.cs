28f89dcb312f21e5f902685ce6228afaa3d369a6
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-07-28 13:53:54 -0300

    feat: metodos CRUD

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new DataContext())
+{
+    // var tag = new Tag { Name = ".NET", Slug = "dotnet"};
+    // context.Tags.Add(tag);
+    // context.SaveChanges();
+
+    // var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet"};
+    // context.Tags.Add(tag2);
+    // context.SaveChanges();
+
+    // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+    // tag.Name = ".Net";
+    // tag.Slug = "dotnet";
+    // context.Update(tag);
+    // context.SaveChanges();
+
+    // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+    // context.Tags.Remove(tag);
+    // context.SaveChanges();
+
+    // var tags = context.Tags.AsNoTracking().ToList();
+    // foreach (var tag in tags)
+    // {
+    //     System.Console.WriteLine(tag.Name);
+    // }
+}
